f9c0bb7ee154f093ceebd12a0b7fa78283ba7ea9
Alex Spirgel <spirgel.alexander@gmail.com>
2016-02-14 18:06:19 -0500

    large update heading towards a finished version 1.10

--- inviewport.js
+++ inviewport.js
@@ -2,23 +2,23 @@
 * inViewportJS *
 ****************/
 /*
-Version: 1.01 - 2/9/2016
+Version: 1.10 - 2/14/2016
 Written By: Alexander Spirgel - alexanderspirgel.com
 */
 ;(function($){
     var defaults = {
-        type: 'isInVeiw', // 'inView', 'inFullView'
-        viewportpadding: '0 0 0 0', // CSS style input viewport padding, accepts negative values
-        elementPadding: '0 0 0 0', // CSS style input element padding, accepts negative values
-        enterDelay: 0, // Delay in miliseconds for enter trigger
-        leaveDelay: 0, // Delay in miliseconds for leave trigger
-        enterCallback: function(){
+        'type': 'isInView', // 'inView', 'inFullView'
+        'elementPadding': '0 0 0 0', // CSS style input element padding, accepts negative values
+        'viewportpadding': '0 0 0 0', // CSS style input viewport padding, accepts negative values
+        'enterDelay': 0, // Delay in miliseconds for enter trigger
+        'leaveDelay': 0, // Delay in miliseconds for leave trigger
+        'enterCallback': function(){
             // Enter callback code here
         },
-        leaveCallback: function(){
+        'leaveCallback': function(){
             // Leave callback code here
         },
-        scrollInterval: 100 // Interval in miliseconds to check when scrolling
+        'scrollInterval': 100 // Interval in miliseconds to check when scrolling
     }
     $.fn.isInView = function(options){
         // Check for target element
@@ -28,28 +28,47 @@ Written By: Alexander Spirgel - alexanderspirgel.com
             this.each(function(){$(this).isInView(options)});
             return this;
         }
-        // Merge options with defaults
-        options = $.extend(defaults,options);
-        console.log(options)
-        function validateOptionInput(option,input){
+        // Set a reference to chosen element
+        var elem = this;
+
+        /*
+        $(window).bind('resize', resizeWindow);
+        $(window).unbind('resize', resizeWindow);
+        */
+
+        // Initialize
+        var init = function(){
+            // Merge options with defaults
+            var mergedOptions = $.extend(defaults,options);
+            console.log(mergedOptions)
+            console.log(checkInView(elem, mergedOptions));
+        }
+
+        // Split by ' ' and remove nulls
+        function splitSpace(inputStr){
+            var inputArray = inputStr.split(" ");
+            // Remove empty array values caused by excessive spaces
+            inputArray = inputArray.filter(Boolean)
+            return inputArray;
+        }
+        // Input validation
+        function validateOptionInput(option, input){
             var passFlag = false;
-            if(option == "type"){
-                if(input == "isInView" || input == "isInFullView"){
+            if(option == 'type'){
+                if(input == 'isInView' || input == 'isInFullView'){
                     passFlag = true;
                 }
             }
-            if(option == "viewportpadding" || option == "elementPadding"){
-                var inputArray = input.split(" ");
-                // Remove empty array values caused by excessive spaces
-                inputArray = inputArray.filter(Boolean)
+            else if(option == "viewportpadding" || option == "elementPadding"){
+                var inputArray = splitSpace(input);
                 // Check for the right amount of values and that all characters are digits or spaces
                 if(inputArray.length >= 1 && inputArray.length <= 4 && input.match(/(?=\D)(\S)/g) == null){
                     passFlag = true;
                 }
             }
-            if(option == "enterDelay" || option == "leaveDelay" || option == "scrollInterval"){
-                // Check that all characters are digits
-                if(input.match(/(\D)/g) == null){
+            else if(option == "enterDelay" || option == "leaveDelay" || option == "scrollInterval"){
+                // Check that input is an integer
+                if(input === parseInt(input, 10)){
                     passFlag = true;
                 }
             }
@@ -59,6 +78,70 @@ Written By: Alexander Spirgel - alexanderspirgel.com
             }
             return passFlag;
         }
+        // CSS Style formatting
+        function formatCSS(str){
+            var arrLabels = ["topVal","rightVal","bottomVal","leftVal"];
+            var arrVals = splitSpace(str);
+            arrVals = arrVals.filter(Boolean)
+            if(arrVals.length == 1){
+                arrVals[1] = arrVals[2] = arrVals[3] = arrVals[0];
+            }
+            else if(arrVals.length == 2){
+                arrVals[2] = arrVals[0];
+                arrVals[3] = arrVals[1];
+            }
+            else if(arrVals.length == 3){
+                arrVals[3] = arrVals[1];
+            }
+            var objOut = {};
+            for(i = 0;i < 4;i++){
+                objOut[arrLabels[i]] = parseInt(arrVals[i]);
+            }
+            return objOut;
+        }
+        // Check if elem is within viewport
+        function checkInView(elem, options){
+            // If all relevant options are valid
+            if(validateOptionInput("type", options["type"]) &&
+            validateOptionInput("elementPadding", options["elementPadding"]) &&
+            validateOptionInput("viewportpadding", options["viewportpadding"]) &&
+            validateOptionInput("enterDelay", options["enterDelay"]) &&
+            validateOptionInput("leaveDelay", options["leaveDelay"]) &&
+            validateOptionInput("scrollInterval", options["scrollInterval"])){
+                var elementPaddingObj = formatCSS(options["elementPadding"]);
+                var viewportpaddingObj = formatCSS(options["viewportpadding"]);
+                // Convert from JQuery selector to normal JavaScript
+                if (typeof jQuery === "function" && elem instanceof jQuery){
+                    elem = elem[0];
+                }
+                // Set variable with outer boundaries of elem
+                var rect = elem.getBoundingClientRect();
+                if(options["type"] == "isInFullView"){
+                    // Return true if all boundaries are within viewport, accounting for padding
+                    return (
+                        (rect.top - elementPaddingObj["topVal"]) >= (0 - viewportpaddingObj["topVal"]) &&
+                        (rect.bottom + elementPaddingObj["bottomVal"]) <= (window.innerHeight + viewportpaddingObj["bottomVal"]) &&
+                        (rect.left - elementPaddingObj["leftVal"]) >= (0 - viewportpaddingObj["leftVal"]) &&
+                        (rect.right + elementPaddingObj["rightVal"]) <= (window.innerWidth + viewportpaddingObj["rightVal"])
+                    );
+                }
+                else{
+                    // Return true if one or more boundary is within viewport, accounting for padding
+                    return (
+                        ( 
+                            (rect.top - elementPaddingObj["topVal"]) < (window.innerHeight + viewportpaddingObj["bottomVal"]) &&
+                            (rect.bottom + elementPaddingObj["bottomVal"]) > (0 - viewportpaddingObj["topVal"])
+                        ) &&
+                        (
+                            (rect.left - elementPaddingObj["leftVal"]) < (window.innerWidth + viewportpaddingObj["rightVal"]) &&
+                            (rect.right + elementPaddingObj["rightVal"]) > (0 - viewportpaddingObj["leftVal"])
+                        )        
+                    );
+                }
+            }
+        }
+        // Run it
+        init();
     }
 })(jQuery);
 /*
