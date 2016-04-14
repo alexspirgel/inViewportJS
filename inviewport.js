402c589efde796c7247fed28ad8bb84eb5677771
Alex Spirgel <spirgel.alexander@gmail.com>
2016-04-13 21:40:26 -0400

    1.2 Update

--- inviewport.js
+++ inviewport.js
@@ -8,11 +8,7 @@ Written By: Alexander Spirgel - alexanderspirgel.com
 ;(function($){
     // Default set of options that will be used unless set upon initialization
     var defaults = {
-        'type': 'isInView', // 'inView', 'inFullView'
-        'elementOffsetTop': 0, // Element offset top, accepts negative values
-        'elementOffsetRight': 0, // Element offset right pixel value, accepts negative values
-        'elementOffsetBottom': 0, // Element offset bottom pixel value, accepts negative values
-        'elementOffsetLeft': 0, // Element offset left pixel value, accepts negative values
+        'type': 'inView', // 'inView', 'inFullView'
         'viewportOffsetTop': 0, // Viewport offset top pixel value, accepts negative values
         'viewportOffsetRight': 0, // Viewport offset right pixel value, accepts negative values
         'viewportOffsetBottom': 0, // Viewport offset bottom pixel value, accepts negative values
@@ -23,14 +19,14 @@ Written By: Alexander Spirgel - alexanderspirgel.com
         'leaveCallback': function(){
             // Leave callback code here
         },
-        'checkInterval': 100 // Frequency interval in miliseconds to check
+        'checkInterval': 200 // Frequency interval in miliseconds to check
     }
     $.fn.isInView = function(options){
         // Check for target element
         if(this.length == 0) return this;
         // Support mutltiple elements
         if(this.length > 1){
-            this.each(function(){$(this).isInView(options)});
+            this.each(function(){$(this).inView(options)});
             return this;
         }
         // Set a reference to chosen element
@@ -54,7 +50,7 @@ Written By: Alexander Spirgel - alexanderspirgel.com
                     // Get the current Time
                     var now = +new Date;
                     // If enough time has passed
-                    if (now - lastScroll > mergedOptions["checkInterval"]){
+                    if (now - lastScroll > mergedOptions['checkInterval']){
                         // Run check on scroll
                         adjustClasses(elem, checkInView(elem, mergedOptions), mergedOptions);
                         // Reset scroll time
@@ -66,7 +62,7 @@ Written By: Alexander Spirgel - alexanderspirgel.com
                     // Get the current Time
                     var now = +new Date;
                     // If enough time has passed
-                    if (now - lastResize > mergedOptions["checkInterval"]){
+                    if (now - lastResize > mergedOptions['checkInterval']){
                         // Run check on resize
                         adjustClasses(elem, checkInView(elem, mergedOptions), mergedOptions);
                         // Reset scroll time
@@ -82,17 +78,17 @@ Written By: Alexander Spirgel - alexanderspirgel.com
             var passFlag = false;
             if(option == 'type'){
                 // Check for valid in view type
-                if(input == 'isInView' || input == 'isInFullView'){
+                if(input == 'inView' || input == 'inFullView'){
                     passFlag = true;
                 }
             }
-            else if(option == "elementOffsetTop" || option == "elementOffsetRight" || option == "elementOffsetBottom" || option == "elementOffsetLeft" || option == "viewportOffsetTop" || option == "viewportOffsetRight" || option == "viewportOffsetBottom" || option == "viewportOffsetLeft" || option == "checkInterval"){
+            else if(option == 'viewportOffsetTop' || option == 'viewportOffsetRight' || option == 'viewportOffsetBottom' || option == 'viewportOffsetLeft' || option == 'checkInterval'){
                 // Check that input is an integer
                 if(!isNaN(input) && parseInt(Number(input)) == input && !isNaN(parseInt(input, 10))){
                     passFlag = true;
                 }
             }
-            else if(option == "enterCallback" || option == "leaveCallback"){
+            else if(option == 'enterCallback' || option == 'leaveCallback'){
                 // Do nothing. These values should be javascript which should throw their own errors
                 passFlag = true;
             }
@@ -105,63 +101,62 @@ Written By: Alexander Spirgel - alexanderspirgel.com
         // Check if elem is within viewport
         function checkInView(elem, mergedOptions){
             // Convert from JQuery selector to normal JavaScript
-            if (typeof jQuery === "function" && elem instanceof jQuery){
+            if (typeof jQuery === 'function' && elem instanceof jQuery){
                 elem = elem[0];
             }
             // Set variable with outer boundaries of elem
             var rect = elem.getBoundingClientRect();
-            if(mergedOptions["type"] == "isInFullView"){
+            if(mergedOptions['type'] == 'inFullView'){
                 // Return true if all boundaries are within viewport, accounting for offset
                 return (
-                    (rect.top) >= (0) &&
-                    (rect.bottom) <= (window.innerHeight) &&
-                    (rect.left) >= (0) &&
-                    (rect.right) <= (window.innerWidth)
+                    (rect.top) >= (0 + mergedOptions['viewportOffsetTop']) &&
+                    (rect.bottom) <= (window.innerHeight - mergedOptions['viewportOffsetBottom']) &&
+                    (rect.left) >= (0 + mergedOptions['viewportOffsetLeft']) &&
+                    (rect.right) <= (window.innerWidth - mergedOptions['viewportOffsetRight'])
                 );
             }
-            else{
+            else{ // inView
                 // Return true if one or more boundary is within viewport, accounting for offset
                 return (
                     ( 
-                        (rect.top) < (window.innerHeight) &&
-                        (rect.bottom) > (0)
+                        (rect.top) < (window.innerHeight - mergedOptions['viewportOffsetBottom']) &&
+                        (rect.bottom) > (0 + mergedOptions['viewportOffsetTop'])
                     ) &&
                     (
-                        (rect.left) < (window.innerWidth) &&
-                        (rect.right) > (0)
+                        (rect.left) < (window.innerWidth - mergedOptions['viewportOffsetRight']) &&
+                        (rect.right) > (0 + mergedOptions['viewportOffsetLeft'])
                     )        
                 );
             }
         }
         function adjustClasses(elem, isVisible, options){
-            //adjust classes
             if(isVisible){
-                if(!elem.hasClass('ivp-viewing')){
-                    elem.addClass('ivp-viewing');
+                if(!elem.hasClass('vp-viewing')){
+                    elem.addClass('vp-viewing');
                     // Run enter callback
                    options.enterCallback();
                 }
-                if(!elem.hasClass('ivp-viewed')){
-                    elem.addClass('ivp-viewed');
+                if(!elem.hasClass('vp-viewed')){
+                    elem.addClass('vp-viewed');
                 }
-                if(elem.hasClass('ivp-unviewed')){
-                    elem.removeClass('ivp-unviewed');
+                if(elem.hasClass('vp-unviewed')){
+                    elem.removeClass('vp-unviewed');
                 }
             }
             else{
-                if(elem.hasClass('ivp-viewing')){
-                    elem.removeClass('ivp-viewing');
+                if(elem.hasClass('vp-viewing')){
+                    elem.removeClass('vp-viewing');
                     // Run leave callback
                     options.leaveCallback();
                 }
-                if(!elem.hasClass('ivp-viewed') && !elem.hasClass('ivp-unviewed')){
-                    elem.addClass('ivp-unviewed');
+                if(!elem.hasClass('vp-viewed') && !elem.hasClass('vp-unviewed')){
+                    elem.addClass('vp-unviewed');
                 }
             }
             /*
-                'ivp-unviewed' - never been viewed
-                'ivp-viewing' - currently in view
-                'ivp-viewed' - has been viewed
+                'vp-unviewed' - never been viewed
+                'vp-viewing' - currently in view
+                'vp-viewed' - has been viewed
             */
         }
         // Run it
